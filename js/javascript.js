79a7596b5580d9731abeaf95259ff021d56d42da
AgustinaMariscotti <116138075+AgustinaMariscotti@users.noreply.github.com>
2022-12-10 13:18:56 -0300

    Add files via upload

---
+++ js/javascript.js
@@ -0,0 +1,13 @@
+function oscuro () {
+  document.body.style.backgroundImage = "none";
+  document.body.style.backgroundColor = "black";
+  document.body.style.color = "white";
+}
+
+function copy (){
+  document.getelementByID("firma")
+}
+
+function copy() {
+    alert ("Pagina realizada por Agustina Mariscotti-Fecha de creación 10/12/2022")
+    }
